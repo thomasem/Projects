bb7ade07f5d8276ca7b9983eaf4fc7dc2628dcc0
Thomas Maddox <thomas.maddox@RACKSPACE.com>
2013-09-06 13:59:23 -0500

    Fibonacci to Nth place

---
+++ Numbers/fibonacci.py
@@ -0,0 +1,21 @@
+import argparse
+from sys import stdout
+
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Fibonacci number to the Nth position in the sequence")
+    parser.add_argument('num', type=int, help="Number of digits of PI to show.")
+    args = parser.parse_args()
+
+    stdout.write(str(fib(0)))
+    stdout.flush()
+    for num in range(1, args.num):
+        stdout.write(" %s" % str(fib(num)))
+        stdout.flush()
