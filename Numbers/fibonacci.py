f6ad6224cea69f005e124a7defda66abd5998da5
Thomas Maddox <thomas.maddox@RACKSPACE.com>
2013-09-06 14:30:32 -0500

    updated fibonacci to use a generator instead

--- Numbers/fibonacci.py
+++ Numbers/fibonacci.py
@@ -1,5 +1,7 @@
 import argparse
 from sys import stdout
+from time import sleep
+
 
 def fib(n):
     if n == 0:
@@ -8,12 +10,41 @@ def fib(n):
         return 1
     return fib(n - 1) + fib(n - 2)
 
+
+def gen_fib():
+    yield 0
+    yield 1
+    a, b = 0, 1
+    while True:
+        yield a + b
+        a, b = b, a + b
+
+
+def display(num):
+    stdout.write("%s " % num)
+    stdout.flush()
+    sleep(0.1)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Fibonacci number to the Nth position in the sequence")
-    parser.add_argument('num', type=int, help="Number of digits of PI to show.")
+    parser.add_argument(
+        'num',
+        type=int,
+        help="Number of digits of PI to show."
+    )
+    parser.add_argument(
+        '--efficient',
+        default=False,
+        help="Use generator method to calculate. Default: False",
+        action="store_true"
+    )
     args = parser.parse_args()
 
-    for num in range(1, args.num):
-        stdout.write("%s " % str(fib(num)))
-        stdout.flush()
+    gen = gen_fib()
+    for num in xrange(args.num):
+        if args.efficient:
+            display(gen.next())
+        else:
+            display(fib(num))
--- Numbers/pi.py
+++ Numbers/pi.py
@@ -3,7 +3,8 @@ from sympy.mpmath import mp
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="PI to the Nth digit")
-    parser.add_argument('num', type=int, help="Number of digits of PI to show.")
+    parser.add_argument('num', type=int,
+                        help="Number of digits of PI to show.")
     args = parser.parse_args()
 
     mp.dps = args.num
